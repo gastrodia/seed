c58fea536b283eaf463c4bfa84201d6f64173781
gastrodia <gastrodia@users.noreply.github.com>
2017-08-12 21:42:18 +0800

    Update and rename install-fast-apt.sh to configure-apt.sh

--- configure-apt.sh
+++ configure-apt.sh
@@ -1,6 +1,8 @@
 sudo apt-get install axel
+wget https://raw.githubusercontent.com/gastrodia/seed/master/apt-fast.sh
 sudo cp apt-fast.sh /usr/bin/apt-fast
-sudo chmod +x apt-fast
+sudo chmod +x /usr/bin/apt-fast
+wget https://raw.githubusercontent.com/gastrodia/seed/master/sources.list
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak 
 sudo cp sources.list /etc/apt/sources.list 
 sudo apt-fast update
